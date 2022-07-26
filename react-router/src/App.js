dc4c4953f56e7a5adad1275bf54bb4cd5c2292b2
Opeyemi Ogundeyi <yemi.ogundeyi@gmail.com>
2022-07-26 22:27:15 +0100

    mk

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from "./components/home";
 import Posts from "./components/posts";
 import Profile from "./components/profile";
@@ -8,10 +8,23 @@ const App = () => {
   return (
     <>
       <BrowserRouter>
+        <header>
+          <Link to="/">Home</Link> - <Link to="/posts">Posts</Link> -
+          <Link
+            to={{
+              pathname: "/profile",
+              hash: "#francis",
+              search: "?true=enabled",
+            }}
+          >
+            Profile
+          </Link>
+          <hr />
+        </header>
         <Routes>
-          <Route exact component={Home} />
-          <Route exact path="/posts" component={Posts} />
-          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/profile/:posts" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </>
--- react-router/src/components/profile.js
+++ react-router/src/components/profile.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  let navigate = useNavigate();
+
+  return (
+    <>
+      <Link
+        onClick={() => {
+          navigate("posts");
+        }}
+      >
+        people profile
+      </Link>
+    </>
+  );
 };
 
 export default Profile;
